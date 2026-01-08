323992d29cc1243fa43d9aa4cc8ba38c8e8e181c
oDevFred <caio.frederico2001@outlook.com>
2026-01-08 15:27:40 -0300

    chore: inicializa projeto SpringBoot com dependências básicas
    - Spring Web, Data JPA, H2, Validation, Lombok
    - Configuração do H2 em memória
    - Estrutura de pacotes (domain, application, api)

---
+++ src/main/java/io/github/odevfred/picpaystudy/PicPayStudyApplication.java
@@ -0,0 +1,13 @@
+package io.github.odevfred.picpaystudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPayStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicPayStudyApplication.class, args);
+    }
+
+}
